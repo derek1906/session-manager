8f83c17498034ba9cc90e97278b8a0fc7df5d893
Derek Leung <derek1906@gmail.com>
2018-11-25 01:39:42 -0600

    Add Angular Material and basic styling

--- projects/dashboard/src/app/app.module.ts
+++ projects/dashboard/src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 import { AppComponent } from './app.component';
+import { DomainListComponent } from './domain-list/domain-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DomainListComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
